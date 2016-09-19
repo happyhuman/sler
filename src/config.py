2e7a8efda7d2c9f445549c2f0d5039bc08d4fca2
Shahin Saadati <saadati@gmail.com>
2016-09-18 21:59:41 -0700

    Simplified ConfigManager and Sler Engine.

--- src/config.py
+++ src/config.py
@@ -1,25 +1,67 @@
 import os.path
 import logging
+import sklearn.svm
+import sklearn.neighbors
+import sklearn.linear_model
+import sklearn.grid_search
+
+
+class EstimatorWrapper(object):
+    _SVC_DEFAULT_H  = {}
+    _SVC_DEFAULT_HP = {'C': [0.01, 1, 100], 'kernel': ['rbf', 'linear']}
+    _REGRESSION_ESTIMATORS = {
+        'linearregression': sklearn.linear_model.LinearRegression,
+
+    }
+    _CLASSIFICATION_ESTIMATORS = {
+        'svc': sklearn.svm.SVC,
+        'knn': sklearn.neighbors.NearestNeighbors,
+    }
+
+    def __init__(self, name, parameters=None, hyperparameters=None, generate='all'):
+        self._type = None
+        self.name = name
+        self.hyperparameters = hyperparameters
+        self.parameters = parameters
+        self.generate = generate
+        if name in EstimatorWrapper._REGRESSION_ESTIMATORS:
+            self._type = 'regression'
+            if parameters is None:
+                self.estimator = EstimatorWrapper._REGRESSION_ESTIMATORS[name]()
+            else:
+                self.estimator = EstimatorWrapper._REGRESSION_ESTIMATORS[name](**parameters)
+        elif name in EstimatorWrapper._CLASSIFICATION_ESTIMATORS:
+            self._type = 'classification'
+            if parameters is None:
+                self.estimator = EstimatorWrapper._CLASSIFICATION_ESTIMATORS[name]()
+            else:
+                self.estimator = EstimatorWrapper._CLASSIFICATION_ESTIMATORS[name](**parameters)
+        else:
+            logging.error("Unknown estimator: %s", name)
+
+    def get_estimator(self):
+        if self.generate == 'all':
+            estimator = sklearn.grid_search.GridSearchCV(self.estimator, self.hyperparameters, n_jobs=2)
+        elif self.generate.startswith('random'):
+            n_iter = int(self.generate[len('random'):]) if self.generate.startswith('random:') else 10
+            estimator = sklearn.grid_search.RandomizedSearchCV(self.estimator, self.hyperparameters, n_iter, n_jobs=2)
+        else:
+            logging.error("Unknown generate parameter: %s. Ignoring hyperparameters", self.generate)
+            estimator = self.estimator
+        return estimator
 
 
 class ConfigManager(object):
     def __init__(self):
-        self.default_hp = {}
-        self.default_hp['svc.default'] = {'C': [0.01, 1, 100], 'kernel': ['rbf', 'linear']}
-        self.input_file = None
-        self.prediction_file = None
-        self.report_file = None
-        self.pre = None
         self.feature_names = None
         self.response_name = None
         self.estimator_type = None
-        self.standardize = None
+        self.rescale = None
         self.vectorize = None
+        self.impute = None
         self.estimators = []
-        self.test_percentage = None
         self.regression_estimators = {'linearregression', }
         self.classification_estimators = {'svc', 'ridge', 'knn'}
-        self.clustering_estimators = {'kmeans'}
 
     def load_yaml(self, yaml_file):
         if os.path.exists(yaml_file):
@@ -30,15 +72,10 @@ class ConfigManager(object):
             logging.error("%s does not exist.", yaml_file)
 
     def _process_config_values(self, cfg):
-        if 'input' in cfg:
-            self._process_input_configuration(cfg['input'])
+        if 'pre' in cfg:
+            self._process_pre_configuration(cfg['pre'])
         else:
             logging.error("The configuration should specify the input")
-            return
-        if 'output' in cfg:
-            self._process_output_configuration(cfg['output'])
-        else:
-            logging.error("The configuration should specify the output")
             return
         if 'estimators' in cfg:
             self._process_estimators_configuration(cfg['estimators'])
@@ -47,61 +84,19 @@ class ConfigManager(object):
 
     def _process_estimators_configuration(self, estimators_cfg):
         logging.debug("Estimator config is %s", estimators_cfg)
-        for estimator_cfg in estimators_cfg:
-            _type = estimator_cfg['type']
-            if _type in self.regression_estimators:
-                self.estimator_type = 'regression'
-            elif _type in self.classification_estimators:
-                self.estimator_type = 'classification'
-            elif _type in self.clustering_estimators:
-                self.estimator_type = 'clustering'
-            if self.estimator_type is None:
-                logging.error("Unknown Estimator: '%s'", _type)
-                return False
-            if self.estimator_type in {'regression', 'classification'}:
-                if self.response_name is None:
-                    logging.error("Response column is not specified")
-                    return False
-            else:
-                if self.response_name is not None:
-                    logging.warn("Response is not required for clustering. Ignoring it.")
-            self.estimators.append(estimator_cfg)
+        for est_cfg in estimators_cfg:
+            est = EstimatorWrapper(est_cfg['type'], est_cfg.get('parameters'), est_cfg.get('hyperparameters'), est_cfg.get('generate'))
+            self.estimators.append(est)
         return True
 
-    def _process_output_configuration(self, output_cfg):
-        if 'predictions' in output_cfg:
-            self.prediction_file = output_cfg['predictions']
-        else:
-            logging.error("No file is specified to write the predictions output.")
-            return False
-        if 'report' in output_cfg:
-            self.report_file = output_cfg['report']
-        return True
-
-    def _process_input_configuration(self, input_cfg):
-        logging.debug("Input config is %s", input_cfg)
-        if 'filename' in input_cfg:
-            self.input_file = input_cfg['filename']
-        else:
-            logging.error("Input filename is not given.")
-            return False
-        if 'features' in input_cfg:
-            self.feature_names = input_cfg['features']
-        if 'response' in input_cfg:
-            self.response_name = input_cfg['response']
-        if 'pre' in input_cfg:
-            self.pre = input_cfg['pre']
-            if 'impute' in self.pre:
-                for key, value in self.pre['impute'].iteritems():
-                    if value not in {'mode', 'median', 'mean'}:
-                        logging.warn("Impute value should be one of mode, median, mean. Unknown value: '%s'.", value)
-        if 'standardize' in input_cfg:
-            self.standardize = input_cfg['standardize']
-        if 'vectorize' in input_cfg:
-            self.vectorize = input_cfg['vectorize']
-        if 'testpercentage' in input_cfg:
-            self.test_percentage = input_cfg['testpercentage']
-            if self.test_percentage < 1 or self.test_percentage > 99:
-                logging.warn("testpercentage should be between 0 and 100. Setting it to 10")
-                self.test_percentage = 10
-        return True
+    def _process_pre_configuration(self, pre_cfg):
+        logging.error("Preprocess config is %s", pre_cfg)
+        self.feature_names = pre_cfg.get('features')
+        self.response_name = pre_cfg.get('response')
+        self.impute = pre_cfg.get('impute')
+        self.rescale = pre_cfg.get('rescale')
+        self.vectorize = pre_cfg.get('vectorize')
+        if self.response_name is None:
+            logging.error("Response should be specified")
+        elif self.feature_names is not None and self.response_name in self.feature_names:
+            logging.error("Response cannot also be one of the features")
--- src/sler.py
+++ src/sler.py
@@ -1,107 +1,94 @@
-import sys
 from config import ConfigManager
 import os.path
 import pandas as pd
 import logging
 import sklearn
-import sklearn.linear_model
-import sklearn.grid_search
-import sklearn.neighbors
 import sklearn.cross_validation
-import sklearn.metrics.accuracy_score
+import sklearn.metrics
+import argparse
+import sklearn.preprocessing
 
 
 class ScikitLearnEasyRunner(object):
-    def __init__(self, config_manager):
-        self.cfgm = config_manager
+    def __init__(self, input_file, config_file, output_file=None, test_percentage=10):
+        self.cfgm = ConfigManager()
+        self.cfgm.load_yaml(config_file)
+        self.input_file = input_file
+        self.output_file=output_file
+        self.test_percentage = test_percentage
         self.dataframe = None
         self.train_features = None
         self.train_response = None
         self.test_features = None
         self.test_response = None
-        self.pred_df = None
-        self.estimators = {}
+        self.estimators = {e.name: e.get_estimator() for e in self.cfgm.estimators}
 
     def _pre_process(self):
-        if os.path.exists(self.cfgm.input_file):
-            if self.cfgm.input_file.endswith('.csv'):
-                self.dataframe = pd.read_csv(self.cfgm.input_file)
-            elif self.cfgm.input_file.endswith('.xlsx'):
-                self.dataframe = pd.read_excel(self.cfgm.input_file)
+        if self._load_input():
+            self.rescale()
+            self._impute()
+            #TODO: Where should I do vectorization?
+            self._prep_features_response()
+            return True
+        return False
+
+    def rescale(self):
+        if self.cfgm.rescale is not None:
+            for item in self.cfgm.rescale:
+                feature = item.keys()[0]
+                value = item[feature]
+                if value == 'standardize':
+                    self.dataframe[feature] = sklearn.preprocessing.StandardScaler().fit_transform(self.dataframe[feature])
+                elif value in {'normalize', 'minmax'}:
+                    self.dataframe[feature] = sklearn.preprocessing.MinMaxScaler().fit_transform(self.dataframe[feature])
+
+    def _impute(self):
+        if self.cfgm.impute is not None:
+            for item in self.cfgm.impute:
+                feature = item.keys()[0]
+                value = item[feature]
+                self._fillna(feature, value)
+
+    def _load_input(self):
+        if os.path.exists(self.input_file):
+            if self.input_file.endswith('.csv'):
+                self.dataframe = pd.read_csv(self.input_file)
+            elif self.input_file.endswith('.xlsx'):
+                self.dataframe = pd.read_excel(self.input_file)
             else:
                 logging.error("Unable to read the input file. It has to be a csv or an xlsx file")
                 return False
-            if self.cfgm.pre is not None:
-                if 'impute' in self.cfgm.pre:
-                    for column, value in self.cfgm.pre['impute']:
-                        self._fillna(column, value)
-            self._prep_features_response()
         else:
             logging.error("Input file '%s' does not exist")
             return False
         return True
 
     def _prep_features_response(self):
-        if self.cfgm.response_name is not None:
-            response = self.dataframe[self.cfgm.response_name].copy()
+        response = self.dataframe[self.cfgm.response_name].copy()
         if self.cfgm.feature_names is None:
             features = self.dataframe.copy()
             del features[self.cfgm.response_name]
         else:
             features = self.dataframe[self.cfgm.feature_names].copy()
-
         features = pd.get_dummies(features)
-
         self.train_features, self.test_features, self.train_response, self.test_response = \
-            sklearn.cross_validation.train_test_split(features, response, test_size=self.cfgm/100.0)
-        self.train_features = pd.get_dummies(self.train_features)
-        self.train_response = self.dataframe[self.cfgm.response_name].copy()
+            sklearn.cross_validation.train_test_split(features, response, test_size=self.test_percentage/100.0)
 
-    def _fillna(self, column, value):
+    def _fillna(self, feature, value):
         if value == 'mean':
-            self.dataframe[column].fillna(self.dataframe[column].mean(), inplace=True)
+            self.dataframe[feature].fillna(self.dataframe[feature].mean(), inplace=True)
         elif value == 'median':
-            self.dataframe[column].fillna(self.dataframe[column].median(), inplace=True)
+            self.dataframe[feature].fillna(self.dataframe[feature].median(), inplace=True)
         elif value == 'mode':
-            self.dataframe[column].fillna(self.dataframe[column].mode(), inplace=True)
+            self.dataframe[feature].fillna(self.dataframe[feature].mode(), inplace=True)
         else:
-            self.dataframe[column].fillna(value, inplace=True)
-
-    def _create_estimators(self):
-        for est in self.cfgm.estimators:
-            _type = est['type']
-            init_params = est['init'] if 'init' in est else {}
-            estimator = self._get_estimator(_type, init_params)
-            gen = est['generate']
-            param_grid = {}
-            if 'hyperparameter' in est:
-                param_grid = est['hyperparameter']
-            elif "%s.default"%_type in self.cfgm.hp:
-                param_grid = self.cfgm.hp["%s.default"%_type]
-            if gen == 'all':
-                searchCV = sklearn.grid_search.GridSearchCV(estimator, param_grid, n_jobs=2)
-            elif gen.startswith('random'):
-                n_iter = int(gen[len('random'):]) if gen.startswith('random:') else 10
-                searchCV = sklearn.grid_search.RandomizedSearchCV(estimator, param_grid, n_iter, n_jobs=2)
-            self.estimators[_type] = searchCV
-
-    def _get_estimator(self, _type, init_params):
-        estimator = None
-        if _type == 'svc':
-            estimator = sklearn.svm.SVC(**init_params)
-        elif _type == 'linearregression':
-            estimator = sklearn.linear_model.LinearRegression(**init_params)
-        elif _type == 'knn':
-            estimator = sklearn.neighbors.NearestNeighbors(**init_params)
-        if estimator is None:
-            logging.error("Unknown estimator type: %s", _type)
-        return estimator
+            self.dataframe[feature].fillna(value, inplace=True)
 
     def fit(self):
         for est in self.estimators.values():
             est.fit(self.train_features, self.train_response)
 
-    def predict(self):
+    def OLD_predict(self):
         self.pred_df = pd.DataFrame()
         for _type, est in self.estimators.iteritems():
             self.pred_df[_type] = est
@@ -111,35 +98,36 @@ class ScikitLearnEasyRunner(object):
             self.pred_df['ensemble'] = self.pred_df.mean(axis=1)
         self.pred_df['actual'] = self.test_response
 
-    def write(self):
+    def report(self):
         print "Writing predictions to %s...."%self.cfgm.prediction_file
         self.pred_df.to_csv(self.cfgm.prediction_file)
         print "Writing report to %s...." % self.cfgm.report_file
-        rep_file = file(self.cfgm.report_file, 'wt')
-        for _type in self.estimators:
-            score = sklearn.metrics.accuracy_score(self.pred_df['actual'], self.pred_df[_type])
-            rep_file.write("Accuracy Score for %s: %f\n"%(_type, score))
-        rep_file.close()
-        print "Finished."
+        for name in self.estimators:
+            score = sklearn.metrics.accuracy_score(self.pred_df['actual'], self.pred_df[name])
+            print "Accuracy Score for %s: %f\n"%(name, score)
+            if hasattr(self.estimators[name], 'best_params_'):
+                print "Best parameters for %s: %s"%(name, self.estimators[name].best_params_)
+            print
 
     def run(self):
         if self._pre_process():
-            self._create_estimators()
             self.fit()
-            self.predict()
-            self.write()
+            #self.predict()
+            self.report()
 
 
 if __name__ == '__main__':
-    args = sys.argv
-    if len(args) < 2:
-        print "Please specify a config file"
-        sys.exit(1)
-    cfgm = ConfigManager()
-    cfgm.load_yaml(args[1])
-    runner = ScikitLearnEasyRunner(cfgm)
-    runner.run()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input_file", help='input file, either a csv or an xlsx file')
+    parser.add_argument("config_file", help='config file, given as a yaml')
+    parser.add_argument("--output", help='output file')
+    parser.add_argument("--testpercentage", help='percentage of rows used for testing. default is 10', type=int)
 
-    print "Done"
+    args = parser.parse_args()
+    output_file = args.output
+    test_percentage = 10 if args.testpercentage is None else args.testpercentage
+    print args.input_file, args.config_file, output_file, test_percentage
+    runner = ScikitLearnEasyRunner(args.input_file, args.config_file)
+    runner.run()
 
 
